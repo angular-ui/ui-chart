fb8262541f98206b1e872d5938c2486b6e081be5
Wesley Cho <wesley.cho@gmail.com>
2013-07-16 18:54:19 -0400

    Added Scatter Chart to demo

--- demo/demo.js
+++ demo/demo.js
@@ -2,7 +2,7 @@
 
 angular.module('ui.chart')
   .controller('DemoCtrl', function ($scope, $chart) {
-    $scope.chartData = [
+    $scope.chart1Data = [
       ['Animals', 'Favorite %'],
       ['Giant Panda', 10],
       ['Shark', 5],
@@ -12,14 +12,29 @@ angular.module('ui.chart')
       ['Pig', 5]
     ];
 
-    $scope.chartOptions = {
+    $scope.chart2Data = [
+      ['Age', 'Weight'],
+      [8, 12],
+      [4, 5.5],
+      [11, 14],
+      [4, 5],
+      [3, 3.5],
+      [6.5, 7]
+    ];
+
+    $scope.chart1 = {
       type: 'PieChart',
-      data: $scope.chartData
+      data: $scope.chart1Data
     };
 
-    $scope.drawChart = function (type, target) {
-      $scope.chartOptions.type = type;
-      $scope.chartOptions.target = target;
-      $chart.drawChart($scope.chartOptions);
+    $scope.chart2 = {
+      type: 'ScatterChart',
+      data: $scope.chart2Data
+    };
+
+    $scope.drawChart = function (type, target, chart) {
+      $scope[chart].type = type;
+      $scope[chart].target = target;
+      $chart.drawChart($scope[chart]);
     };
   });
