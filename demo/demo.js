b311153a1d9d6ff44c2d1056e16c357cd569d52c
Wesley Cho <wesley.cho@gmail.com>
2013-07-16 19:11:45 -0400

    Added more example charts

--- demo/demo.js
+++ demo/demo.js
@@ -22,6 +22,39 @@ angular.module('ui.chart')
       [6.5, 7]
     ];
 
+    $scope.chart3Data = [
+      ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+      ['2004/05',  165,      938,         522,             998,           450,      614.6],
+      ['2005/06',  135,      1120,        599,             1268,          288,      682],
+      ['2006/07',  157,      1167,        587,             807,           397,      623],
+      ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+      ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+    ];
+
+    $scope.chart4Data = [
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ];
+
+    $scope.chart5Data = [
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ];
+
+    $scope.chart6Data = [
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ];
+
     $scope.chart1 = {
       type: 'PieChart',
       data: $scope.chart1Data
@@ -29,7 +62,51 @@ angular.module('ui.chart')
 
     $scope.chart2 = {
       type: 'ScatterChart',
-      data: $scope.chart2Data
+      data: $scope.chart2Data,
+      options: {
+        title: 'Age vs. Weight comparison',
+        hAxis: {title: 'Age', minValue: 0, maxValue: 15},
+        vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+        legend: 'none'
+      }
+    };
+
+    $scope.chart3 = {
+      type: 'ComboChart',
+      data: $scope.chart3Data,
+      options: {
+        title : 'Monthly Coffee Production by Country',
+        vAxis: {title: "Cups"},
+        hAxis: {title: "Month"},
+        seriesType: "bars",
+        series: {5: {type: "line"}}
+      }
+    };
+
+    $scope.chart4 = {
+      type: 'LineChart',
+      data: $scope.chart4Data,
+      options: {
+        title: 'Company Performance'
+      }
+    };
+
+    $scope.chart5 = {
+      type: 'BarChart',
+      data: $scope.chart5Data,
+      options: {
+        title: 'Company Performance',
+        vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
+      }
+    };
+
+    $scope.chart6 = {
+      type: 'ColumnChart',
+      data: $scope.chart6Data,
+      options: {
+        title: 'Company Performance',
+        hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}
+      }
     };
 
     $scope.drawChart = function (type, target, chart) {
