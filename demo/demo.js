cf2c3048499f768a1fc42b4bdbff8e798a995b1b
Wesley Cho <wesley.cho@gmail.com>
2013-07-18 17:03:01 -0400

    Improved demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,23 @@
 angular.module('myChartingApp', ['ui.chart'])
-  .controller('DemoCtrl', function ($scope) {
-    $scope.someData = function () {
-      return [
-        [1,2,3],
-        [4,5,6]
-      ];
-    };
+  .value('charting', {
+    pieChartOptions: { 
+      seriesDefaults: {
+        // Make this a pie chart.
+        renderer: jQuery.jqplot.PieRenderer, 
+        rendererOptions: {
+          // Put data labels on the pie slices.
+          // By default, labels show the percentage of the slice.
+          showDataLabels: true
+        }
+      }, 
+      legend: { show:true, location: 'e' }
+    }
+  })
+  .controller('DemoCtrl', function ($scope, charting) {
+    $scope.someData = [[
+      ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14], 
+      ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+    ]];
+
+    $scope.myChartOpts = charting.pieChartOptions;
   });
