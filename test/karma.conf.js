7318ad7aac89763a9a94e6c9d6562bb5e4ad465e
Wesley Cho <wesley.cho@gmail.com>
2014-04-17 17:02:15 -0400

    Fix jquery reference in karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -9,7 +9,7 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'bower_components/jquery/jquery.js',
+  'bower_components/jquery/dist/jquery.js',
   'bower_components/jqplot/jquery.jqplot.js',
   'bower_components/angular/angular.js',
   'bower_components/angular-mocks/angular-mocks.js',
